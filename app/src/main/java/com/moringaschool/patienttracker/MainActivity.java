44ab2d1c74e4a26641a6911e154d18fafef8b562
Gladys-nimo <wkanyora10@gmail.com>
2021-03-24 10:56:07 +0300

    changed an image and added a toast

--- app/src/main/java/com/moringaschool/patienttracker/MainActivity.java
+++ app/src/main/java/com/moringaschool/patienttracker/MainActivity.java
@@ -3,12 +3,27 @@ package com.moringaschool.patienttracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+         Button button = (Button) findViewById(R.id.addPatientButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "Add patient", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
+//
+//    private class onClickListener implements View.OnClickListener {
+//    }
 }
