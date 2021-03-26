150fd63ad0016faafd67cb0bef02cb5c432c1755
Gladys-nimo <wkanyora10@gmail.com>
2021-03-27 01:19:11 +0300

    edited my activities

--- app/src/main/java/com/moringaschool/patienttracker/MainActivity.java
+++ app/src/main/java/com/moringaschool/patienttracker/MainActivity.java
@@ -2,23 +2,33 @@ package com.moringaschool.patienttracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
+    private Button mFindHospitalButton;
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         Button button = (Button) findViewById(R.id.addPatientButton);
-        button.setOnClickListener(new View.OnClickListener() {
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+        mFindHospitalButton = (Button) findViewById(R.id.findHospitalButton);
+       mFindHospitalButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
             public void onClick(View v) {
-            Toast.makeText(MainActivity.this, "Add patient", Toast.LENGTH_LONG).show();
+                String location = mLocationEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, HospitalActivity.class);
+                intent.putExtra("location", location);
+                startActivity(intent);
             }
         });
 
