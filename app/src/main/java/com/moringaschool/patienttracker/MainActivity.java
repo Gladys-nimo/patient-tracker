42fe9aab3d0f1cae87b57e4b6f2a62a867405fac
Gladys-nimo <wkanyora10@gmail.com>
2021-03-31 01:41:09 +0300

    changed the UI

--- app/src/main/java/com/moringaschool/patienttracker/MainActivity.java
+++ app/src/main/java/com/moringaschool/patienttracker/MainActivity.java
@@ -9,36 +9,44 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.findHospitalButton) Button mFindHospitalButton;
-    @BindView(R.id.location) EditText mLocation;
-    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
+   @BindView(R.id.button) Button submit;
+   @BindView(R.id.editText1) EditText name;
+   @BindView(R.id.editText2) EditText password;
+   @BindView(R.id.editText3) EditText email;
+   @BindView(R.id.editText4) EditText date;
+   @BindView(R.id.editText5) EditText contact;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+       ButterKnife.bind(this);
 
-        mFindHospitalButton.setOnClickListener(this);
-        }
-
+        submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (v == mFindHospitalButton) {
-                    String location = mLocation.getText().toString();
-                    Intent intent = new Intent(MainActivity.this, HospitalActivity.class);
-
-                    intent.putExtra("location", location);
-                    startActivity(intent);
+                if (name.getText().toString().isEmpty() || password.getText().toString().isEmpty() || email.getText().toString().isEmpty() || date.getText().toString().isEmpty()
+                        || contact.getText().toString().isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "Enter the Data", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Name -  " + name.getText().toString() + " \n" + "Password -  " + password.getText().toString()
+                            + " \n" + "E-Mail -  " + email.getText().toString() + " \n" + "Date -  " + date.getText().toString()
+                            + " \n" + "Contact -  " + contact.getText().toString(), Toast.LENGTH_SHORT).show();
                 }
+                Intent intent = new Intent(MainActivity.this, HospitalActivity.class);
+                startActivity(intent);
             }
+        });
+    }
 }
 
 
