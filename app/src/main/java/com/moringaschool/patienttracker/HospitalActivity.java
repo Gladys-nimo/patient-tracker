599106d32f3e004c6e1f2b808e5596de0a678a64
Gladys-nimo <wkanyora10@gmail.com>
2021-03-30 13:48:04 +0300

    added a new layout

--- app/src/main/java/com/moringaschool/patienttracker/HospitalActivity.java
+++ app/src/main/java/com/moringaschool/patienttracker/HospitalActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.content.Intent;
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -26,7 +29,6 @@ import butterknife.ButterKnife;
     ,"Bliss Medical Center", "MediHeal Group of hospitals", "SavannahHealthCare","Getrude", "Agakhan",
             "Kenyatta", "Nairobi women's", "Nairobi west", "caren hospital","Bristol Park", "Balozi hopsital", "WentWorth","Neema","PlainsView","St Mary Health"};
 
-    private String [] locations = new String[]{"WestLands","UpperHill","NgongRoad","Embakasi", "Thika Highway","nakuru","Nyeri","Mombasa","Umoja","Roysambu"};
 
 
     @Override
@@ -36,16 +38,7 @@ import butterknife.ButterKnife;
         ButterKnife.bind(this);
 
 
-        MyHospitalsArrayAdapter adapter = new MyHospitalsArrayAdapter(this, android.R.layout.simple_list_item_1, hospitals, locations);
-        mListView.setAdapter(adapter);
 
-       mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-         @Override
-           public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-             String hospital = ((TextView)view).getText().toString();
-             Toast.makeText(HospitalActivity.this, hospital, Toast.LENGTH_LONG).show();
-         }
-       });
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
        mLocationTextView.setText("Here are the hospitals near:" + location);
