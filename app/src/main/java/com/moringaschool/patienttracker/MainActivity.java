92e8e8a80f38fe23347293a0155392135f8158a5
Gladys-nimo <wkanyora10@gmail.com>
2021-03-31 10:34:17 +0300

    changed errors

--- app/src/main/java/com/moringaschool/patienttracker/MainActivity.java
+++ app/src/main/java/com/moringaschool/patienttracker/MainActivity.java
@@ -9,27 +9,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 
-<<<<<<< HEAD
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-=======
+
 public class MainActivity extends AppCompatActivity {
 
-   @BindView(R.id.button) Button submit;
-   @BindView(R.id.editText1) EditText name;
-   @BindView(R.id.editText2) EditText location;
-   @BindView(R.id.editText3) EditText email;
-   @BindView(R.id.editText4) EditText date;
-   @BindView(R.id.editText5) EditText contact;
->>>>>>> week1
+    @BindView(R.id.button) Button submit;
+    @BindView(R.id.editText1) EditText name;
+    @BindView(R.id.editText2) EditText password;
+    @BindView(R.id.editText3) EditText email;
+    @BindView(R.id.editText4) EditText date;
+    @BindView(R.id.editText5) EditText contact;
 
-    @BindView(R.id.findHospitalButton) Button mFindHospitalButton;
-    @BindView(R.id.locationEditText) EditText mLocationEditText;
-    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,32 +32,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        mFindHospitalButton.setOnClickListener(this);
-        }
-
+        submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-<<<<<<< HEAD
-                if (v == mFindHospitalButton) {
-                    String location = mLocationEditText.getText().toString();
-                    Intent intent = new Intent(MainActivity.this, HospitalActivity.class);
-
-                    intent.putExtra("location", location);
-                    startActivity(intent);
-                }
-=======
-                if (name.getText().toString().isEmpty() || location.getText().toString().isEmpty() || email.getText().toString().isEmpty() || date.getText().toString().isEmpty()
+                if (name.getText().toString().isEmpty() || password.getText().toString().isEmpty() || email.getText().toString().isEmpty() || date.getText().toString().isEmpty()
                         || contact.getText().toString().isEmpty()) {
                     Toast.makeText(getApplicationContext(), "Enter the Data", Toast.LENGTH_SHORT).show();
                 } else {
-                    Toast.makeText(getApplicationContext(), "Name -  " + name.getText().toString() + " \n" + "Password -  " + location.getText().toString()
+                    Toast.makeText(getApplicationContext(), "Name -  " + name.getText().toString() + " \n" + "Password -  " + password.getText().toString()
                             + " \n" + "E-Mail -  " + email.getText().toString() + " \n" + "Date -  " + date.getText().toString()
                             + " \n" + "Contact -  " + contact.getText().toString(), Toast.LENGTH_SHORT).show();
                 }
                 Intent intent = new Intent(MainActivity.this, PatientActivity.class);
                 startActivity(intent);
->>>>>>> week1
             }
+        });
+    }
 }
 
 
